786a4c6c8b5df5fed051845e926cd9c69359025a
dreamsicl <vphong@ucsd.edu>
2017-01-31 19:53:33 -0800

    output scraped club data to clubs.json

--- UCSDClubswebscrape.py
+++ UCSDClubswebscrape.py
@@ -5,32 +5,31 @@
 
 import requests
 from bs4 import BeautifulSoup
-
+import json
+import pprint
 
 # In[101]:
 
+pp = pprint.PrettyPrinter(indent=2)
 url = "http://studentorg.ucsd.edu/"
 
-
-# In[8]:
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "lxml")
 
-
-# In[100]:
-
+clubs = []
 for x in soup.find_all("h2"):
-    print x.text
+    club = {}
+    description_block = x.find_next_sibling("div").find("span")
+    if (description_block):
+        print(description_block.text)
+        club['name'] = x.text
+        club['description'] = description_block.text
+        club = {"name": x.text.strip(), "description": description_block.text}
+        clubs.append(club)
 
 
-# In[99]:
+pp.pprint(clubs)
 
-for y in soup.find_all("span"):
-    print y.text + "\n"
-
-
-# In[ ]:
-
-
-
+with open('clubs.json', 'w') as f:
+    json.dump(clubs, f)
