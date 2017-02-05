6b6796cf6f114e4e28e5eb80114b1f97c3acafde
Ken Lin <xil307@ucsd.edu>
2017-02-05 14:22:19 -0800

    about us page

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -7,31 +7,48 @@ class EventList extends React.Component {
 
     //Generates a list of feedbacks
     getEventList(){
-        return this.props.events.map((event, index) => {
+        return this.props.events.map((event) => { //function(events, index){...}
             let name = event.name;
             let date = event.date;
+            let location = event.location;
             let description = event.description;
 
             let header = (
                 <div>
                     <Badge>{name}</Badge>
                     <Badge>{date}</Badge>
+                    <Badge>{location}</Badge>
                 </div>
             );
 
-            return <Panel key={index} bsStyle='info' header={header}>{description}</Panel>;
+            return <Panel key={name} bsStyle='info' header={header}>{description}</Panel>;
         });
     }
+
+    // const events = [
+    //     {name:'p Day', date:'Feb 20, 2017', location:'PC', description:'Everyone handout to catch pokemons'},
+    //     {name:'Hack Day', date:'Jan 31, 2017', location:'CSE building', description:'Hack into others computer'},
+    //     {name:'Water Fun', date:'Feb 02, 2017', location:'Sun God', description:'Get wet and swag'}
+    // ]
 
     render(){
         return (
             <div className='container'>
-              {this.getEventList()}
+              {this.getEventList()}    {/*returns an array of Panel*/}
             </div>
         );
     }
 
 }
+
+// var numbers = [1,2,3,4,5];
+// console.log(numbers) // [1,2,3,4,5]
+
+// var new_numbers = numbers.map(function(element){
+//     return element + 1;   
+// })
+
+// console.log(new_numbers) //[2,3,4,5,6]
 
 EventList.propTypes = {
     events: React.PropTypes.array.isRequired
