4449c7e424054271a101d00aff9900c6af52b1f4
Ying <yiw273@ucsd.edu>
2017-02-05 14:30:04 -0800

    about page

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { TopBar } from './TopBar';
+
+
+
+class About extends React.Component {
+    render(){
+        return (
+            <div>
+                About Us
+                <hr/>
+            </div>
+        )
+    }
+}
+
+export { About };
