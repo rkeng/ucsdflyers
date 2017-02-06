363f35c8c59eb680262e36eeff4f9ae0bfe9a55b
aravindsridhar66 <aravindsridhar66@gmail.com>
2017-02-05 17:22:22 -0800

    Added Link components
    added link to Register buttons

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
-
+import { Link } from 'react-router';
 
 const wellStyles = {maxWidth: 520, margin: '0 auto 20px'};
 
@@ -11,8 +11,13 @@ class Register extends React.Component{
                 <h1> <center> Register As: </center> </h1>
                 <hr/>
                 <div className="well" style={wellStyles}>
-                    <Button bsStyle="primary" bsSize="large" block> New Student </Button>
-                    <Button bsStyle="primary" bsSize="large" block> New Student Organization </Button>
+                    <Link to='register-student'>
+                        <Button bsStyle="primary" bsSize="large" block> Student </Button>
+                    </Link>
+                    <hr/>
+                    <Link to='register-org'>
+                        <Button bsStyle="primary" bsSize="large" block> Student Organization </Button>
+                    </Link>
                 </div>
             </div>
         )
