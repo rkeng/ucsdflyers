8ab7a4c470e9ca65d54aa263360c3c44483b61bd
dreamsicl <vphong@ucsd.edu>
2017-02-05 22:26:09 -0800

    non-functional feedback page with skeleton validation function

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { FormGroup, FormControl, ControlLabel, PageHeader, HelpBlock } from 'react-bootstrap';
+import { Form, FormGroup, FormControl, ControlLabel, PageHeader, HelpBlock, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 
 class Feedback extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: ''};
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { email: ''};
+        this.onEmailChange = this.onEmailChange.bind(this);
     }
 
-    getValidationState() {
-        const length = this.state.value.length;
+    validateEmail() {
+        const length = this.state.email.length;
         if (length > 10) return 'success';
         else if (length > 5) return 'warning';
         else if (length > 0) return 'error';
     }
 
-    handleChange(e) {
+    onEmailChange(e) {
         this.setState({ value: e.target.value });
     }
 
@@ -25,22 +25,47 @@ class Feedback extends React.Component {
         return (
             <div className="container">
                 <PageHeader>Feedback</PageHeader>
-                <form>
-                    <FormGroup
-                        controlId="formBasicText"
-                        validationState={this.getValidationState()}
-                    >
-                        <ControlLabel>Working example with validation</ControlLabel>
-                        <FormControl
-                            type="text"
-                            value={this.state.value}
-                            placeholder="Enter text"
-                            onChange={this.handleChange}
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>Validation is based on string length.</HelpBlock>
+                <Col sm={12}>
+                <Form horizontal>
+                    <FormGroup 
+                        controlId="email" 
+                        validationState={this.validateEmail()}>
+
+                        <Col componentClass={ControlLabel} sm={2}>
+                            <ControlLabel>E-mail Address</ControlLabel>
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl 
+                                type="email" 
+                                value={this.state.email} 
+                                onChange={this.handleChange}
+                            />
+                            <HelpBlock className="small text-muted">
+                                Validation is based on string length. TODO: E-mail validation function
+                            </HelpBlock>
+                        </Col>
+                        <FormControl.Feedback/>
                     </FormGroup>
-                </form>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Feedback
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl 
+                                componentClass="textarea" 
+                                value={this.state.feedback} 
+                                placeholder="Write your feedback to us here."
+                                style={{height: 400}}
+                            />
+                        </Col>
+                        <FormControl.Feedback/>
+                    </FormGroup>
+                    
+                    <FormGroup className="col-sm-12 text-right">
+                        <Button type="submit">Send</Button>
+                    </FormGroup>
+                </Form>
+                </Col>
             </div>
         )
     }
