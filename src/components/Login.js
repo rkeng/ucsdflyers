792ae5eaf49cd3bf442351acdaa0d41f6e6dc0e1
andyzh95 <andysheng1515@hotmail.com>
2017-02-05 16:04:38 -0800

    styling for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,40 +4,40 @@ import { TopBar } from './TopBar';
 import {FormControl, Checkbox, Button, ControlLabel,
 Grid, Row, Col} from 'react-bootstrap'
 
-    class Login extends React.Component{
-      render(){
+  class Login extends React.Component{
+    render(){
         return(
-   <Grid>
-   <Row className = "show-login">
-   <Col lg={8} lgPush={2}>
-    <form>
-      <ControlLabel>Email address</ControlLabel>
-      <FormControl
-          id="formControlsEmail"
-          type="email"
-          label="Email address"
-          placeholder="Enter email"
-        />
-        <ControlLabel>Password</ControlLabel>
-        <FormControl
-          id="formControlsPassword"
-          label="Password"
-          type="password"
-          placeholder="Enter password"
-        />
-        <Checkbox checked readOnly>
-         Remember me
-       </Checkbox>
-        <Button type="submit">
-          Submit
-        </Button>
-        <Button type="submit">
-          Forget Password
-        </Button>
-    </form>
-    </Col>
-    </Row>
-    </Grid>
+          <Grid>
+            <Row className = "show-login">
+            <Col lg={6} lgPush={3}>
+            <form>
+              <ControlLabel>Email address</ControlLabel>
+              <FormControl
+                id="formControlsEmail"
+                type="email"
+                label="Email address"
+                placeholder="Enter email"
+              />
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                id="formControlsPassword"
+                label="Password"
+                type="password"
+                placeholder="Enter password"
+              />
+              <Checkbox checked readOnly>
+                Remember me
+              </Checkbox>
+              <Button type="submit">
+                Submit
+              </Button>
+              <Button type="submit">
+                Forget Password
+              </Button>
+            </form>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
