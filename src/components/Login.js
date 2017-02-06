c684e98feae7c62c23fa77e418aac9c74ea6ea41
Ken Lin <xil307@ucsd.edu>
2017-02-05 16:11:25 -0800

    fixed small code style with Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,35 +9,29 @@ Grid, Row, Col} from 'react-bootstrap'
         return(
           <Grid>
             <Row className = "show-login">
-            <Col lg={6} lgPush={3}>
-            <form>
-              <ControlLabel>Email address</ControlLabel>
-              <FormControl
-                id="formControlsEmail"
-                type="email"
-                label="Email address"
-                placeholder="Enter email"
-              />
-              <ControlLabel>Password</ControlLabel>
-              <FormControl
-                id="formControlsPassword"
-                label="Password"
-                type="password"
-                placeholder="Enter password"
-              />
-              <Checkbox checked readOnly>
-                Remember me
-              </Checkbox>
-              <Button type="submit">
-                Submit
-              </Button>
-              <Button type="submit">
-                Forget Password
-              </Button>
-            </form>
-          </Col>
-        </Row>
-      </Grid>
+              <Col lg={6} lgPush={3}>
+                <form>
+                  <ControlLabel>Email address</ControlLabel>
+                  <FormControl
+                    id="formControlsEmail"
+                    type="email"
+                    label="Email address"
+                    placeholder="Enter email"
+                  />
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl
+                    id="formControlsPassword"
+                    label="Password"
+                    type="password"
+                    placeholder="Enter password"
+                  />
+                  <Checkbox checked readOnly>Remember me</Checkbox>
+                  <Button type="submit">Submit</Button>
+                  <Button type="submit">Forget Password</Button>
+                </form>
+              </Col>
+            </Row>
+          </Grid>
     )
   }
 }
