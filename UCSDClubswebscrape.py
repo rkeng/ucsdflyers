a23ae907c37441d117854e7459b9a316e7f32e68
Ken Lin <xil307@ucsd.edu>
2017-02-10 08:10:56 -0800

    scape data files

---
+++ UCSDClubswebscrape.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[89]:
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import pprint
+
+# In[101]:
+
+pp = pprint.PrettyPrinter(indent=2)
+url = "http://studentorg.ucsd.edu/"
+
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "lxml")
+
+clubs = []
+for x in soup.find_all("h2"):
+    club = {}
+    description_block = x.find_next_sibling("div").find("span")
+    if (description_block):
+        print(description_block.text)
+        club['name'] = x.text
+        club['description'] = description_block.text
+        club = {"name": x.text.strip(), "description": description_block.text}
+        clubs.append(club)
+
+
+pp.pprint(clubs)
+
+with open('clubs.json', 'w') as f:
+    f.write("{ clubs: ")
+    json.dump(clubs, f)
+    f.write("}")
