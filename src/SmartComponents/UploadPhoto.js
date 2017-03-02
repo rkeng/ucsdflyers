bf1568930d26e0403835384a94695f5c8c9fb6af
dreamsicl <vphong@ucsd.edu>
2017-03-02 01:38:48 -0800

    dropzone

--- src/SmartComponents/UploadPhoto.js
+++ src/SmartComponents/UploadPhoto.js
@@ -1,98 +1,126 @@
 import React from 'react';
-import { NotificationContainer, NotificationManager } from 'react-notifications'
-import { FaImage } from 'react-icons/lib/fa'
-import { FormGroup, Form, Button, ControlLabel, FormControl } from 'react-bootstrap'
+import { NotificationManager } from 'react-notifications'
+import { FaImage, FaClose } from 'react-icons/lib/fa'
+import { Form, Button } from 'react-bootstrap'
 import { firebase } from '../models/FlyersFirebase'
+import Dropzone from 'react-dropzone'
 //import * as storage from '@google-cloud/storage'
 
+// should point to <endpoint>/images/
 const dbImagesRef = firebase.database().ref("images");
 const storage = firebase.storage();
-//console.log(stImagesRef)
+
 
 class UploadPhotoPage extends React.Component {
-    
     constructor(props) {
         super(props)
         this.state = {
-            file: '',
-            imagePreviewUrl: ''
+            preview: {
+                file: null,
+                imagePreviewUrl: null
+            },
+            uploadProgress: null,
+            files: [],
+            currentUser: null
         }
-        this.selectFile = this.selectFile.bind(this);
+        this.deleteFile = this.deleteFile.bind(this);
         this.uploadPhoto = this.uploadPhoto.bind(this);
+        this.onDrop = this.onDrop.bind(this);
     }
     
-    selectFile(e) {
-        e.preventDefault();
-        const that = this;
-        
-        var file = e.target.files[0];
-        let reader = new FileReader();
-        
-        // Check if the file is an image.
-        if (file && !file.type.match('image.*')) {
-            var data = {
-              message: 'You can only share images',
-              timeout: 2000
-            };
-            NotificationManager.error(data.message, "", data.timeout)
-            return;
-        }
-        
-        reader.onloadend = () => {
-            that.setState({file: file, imagePreviewUrl: reader.result});   
-        }
-        
-        reader.readAsDataURL(file);
+    componentWillMount() {
+        const that = this
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) { 
+                that.setState({currentUser: user})
+                console.log(user)
+            }
+        })
     }
     
     uploadPhoto(e) {
         e.preventDefault();
         
-        var file = this.state.file;
+        const that = this
         
+        var files = that.state.files;
         // add image to db
-        dbImagesRef.push({
-          imageUrl: "",
-          photoUrl: "",
-        }).then(function(data) {
+        files.map((file, index) => {
+            dbImagesRef.push({
+                imageUrl: "",
+            }).then(function(data) {
 
-          // Upload the image to Firebase Storage.
-          var filePath = data.key + '_' + file.name;
-          var imageToStorage = storage.ref("event-images/" + filePath).put(file);
-            imageToStorage.on('state_changed', function(snapshot) {
-                // in-progress state changes
-            }, function(error) {
-                // unsuccessful upload
-            }, function() {
-                // successful upload
-                data.update({imageUrl: imageToStorage.snapshot.downloadURL})
-            }
-        )}.bind(this)).catch(function(error) {
-          console.error('There was an error uploading a file to Firebase Storage:', error);
-        });
+              // Upload the image to Firebase Storage.
+              // file will be under <currentUser.uid> folder in Firebase Storage
+              var filePath = that.state.currentUser.uid + '/' + data.key + '_' + file.name;
+                console.log(data)
+              var imageToStorage = storage.ref(filePath).put(file);
+                imageToStorage.on('state_changed', function(snapshot) {
+                    // in-progress state changes
+                    let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    that.setState({uploadProgress: percentage + "%"})
+                }, function(error) {
+                    // unsuccessful upload
+                }, function() {
+                    // successful upload
+                    data.update({imageUrl: imageToStorage.snapshot.downloadURL})
+                }
+            )}).catch(function(error) {
+              NotificationManager.error(
+                  'There was an error uploading a file to Firebase: ' + error,
+                  "Error", 3000
+              );
+            });
+        })
         
     }
     
+    onDrop (files) {
+        const that = this;
+        that.setState({files: files})
+        console.log('Received files: ', files);
+    }
+    
+//    FIXME
+    deleteFile(file, key) {
+        const that = this;
+        console.log('Deleting ' +key + 'th file: ', file)
+        var newFiles = that.state.files.splice(key, 1)
+        that.setState({files: newFiles});
+        console.log(this.state.files)
+    }
+    
     render() {
-        let {imagePreviewUrl} = this.state;
-        let $imagePreview = null;
-        if (imagePreviewUrl) {
-            $imagePreview = (<img src={imagePreviewUrl} className="img-thumbnail" style={{width: 150, height: "auto"}}/> );
-        }
+        let previews = this.state.files.map((file, index) => (
+            <div className="pull-left img-thumbnail" style={{width: 125, position: "relative"}} key={index}>
+                <Button style={{position: "absolute", top: 0, right: 0}} onClick={() => this.deleteFile(file,index)}> 
+                    <FaClose size={20}/> 
+                </Button>
+                <img src={file.preview} style={{width: 125, height: "auto"}} alt={file.name}/>
+            </div>
+        ))
                              
         return(
         
             <Form onSubmit={this.uploadPhoto}>
-                <FormGroup controlId="photo">
-                    <ControlLabel className="btn btn-default"><FaImage size={60}/>
-                        <FormControl type="file" accept="image/*,capture=camera" onChange={this.selectFile} style={{display: "none"}}/>
-                    </ControlLabel>
-                 </FormGroup>
             
-                <Button type="submit" onClick={this.uploadPhoto}>
+                <Dropzone onDrop={this.onDrop}>
+                    <FaImage size={100}/>
+                  <div>Try dropping some files here, or click to select files to upload.</div>
+                </Dropzone>
+                {this.state.files != null ?
+                    <div>{previews}</div>
+                    : null
+                }
+                             
+                             
+                <Button type="submit">
                     Upload
                 </Button>
-                <br/>{$imagePreview}
+                             
+                <p>{this.state.uploadProgress}</p>
+            
+                
             </Form>
         
         );
