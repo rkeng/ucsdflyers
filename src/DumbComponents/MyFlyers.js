f4d5505ba1538d7469c982e58a3d2a5e4af58716
Ken Lin <xil307@ucsd.edu>
2017-03-10 17:33:48 -0800

    wokring on myFlyer page

--- src/DumbComponents/MyFlyers.js
+++ src/DumbComponents/MyFlyers.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import { Flyer } from './Flyer'
+import { ObjectToArray } from '../Commen'
 
 //You should be able to access user info through this.props.user
 //That is just static/local info. If you need to update user info on firebase, you need to
@@ -10,8 +11,16 @@ import { connect } from 'react-redux'
 //It is avaiable in Commen:
 //import { ColCenter } from '../Commen'
 class MyFlyersPage extends React.Component{
-    
+
     render(){
+        console.log('user state', this.props.user)
+        const { FlyersLiked } = this.props.user
+        let flyerArray = ObjectToArray(FlyersLiked)
+        flyerArray.map(
+            (flyer) => {
+                return <Flyer flyer={flyer}/>
+            }
+        )
         return <div> This is MyFlyers Page</div>
     }
 }
