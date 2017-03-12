2fbf703ddf6715398c3ee07017e0b41a1f92daf6
Ken Lin <xil307@ucsd.edu>
2017-03-11 21:16:20 -0800

    my flyers page

--- src/DumbComponents/MyFlyers.js
+++ src/DumbComponents/MyFlyers.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Flyer } from './Flyer'
 import { ObjectToArray } from '../Commen'
-import { Card, CardMedia, CardTitle, CardText } from 'react-toolbox/lib/card';
-import { ColCenter } from '../Commen'
-import { Carousel, Col, Image, Button, Row } from 'react-bootstrap'
 
 //You should be able to access user info through this.props.user
 //That is just static/local info. If you need to update user info on firebase, you need to
@@ -16,62 +13,34 @@ import { Carousel, Col, Image, Button, Row } from 'react-bootstrap'
 class MyFlyersPage extends React.Component{
 
     render(){
-        const {
-            name,
-            location,
-            description,
-            date,
-            images,
-            likes
-        } = this.props.flyer
-        console.log('user state', this.props.user)
+
         const { FlyersLiked } = this.props.user
         //flyer array
         let flyerArray = ObjectToArray(FlyersLiked)
-            flyerArray.map(
-                (flyer) => {
-                    return <Flyer flyer={flyer}/>
-                }
-            )
-        // return <div> This is MyFlyers Page</div>
-        //return(){
-        const imagesArray = ObjectToArray(images)
-        const CarouselItems = imagesArray.map(function(image){
-            return (
-                <Carousel.Item key={image.imageUrl}> 
-                    <Image src={image.imageUrl} width={350} responsive/><br/>
-                </Carousel.Item>
-            )
-        })
-        const carouselInstance = (
-            <Carousel>
-                {CarouselItems}
-            </Carousel>
+        console.log('this.props', this.props)
+        let likedFlyers = (this.props.flyers || []).filter(
+            (flyer) => {
+               return flyerArray.includes(flyer.id)
+            }
         )
-        //}
+        var listOfFlyersLiked = likedFlyers.map(
+            (flyer, index) => {
+                return <Flyer flyer={flyer} key={index}/>
+            }
+        )
 
         return(
-            <Col sm={12} md={3} >
-                <Row>
-                    <Card raised={true} className='raised'>
-                        <CardMedia aspectRatio="wide" children={carouselInstance} />
-                        <CardTitle //title={titleAndBtn}
-                        subtitle={`Date: ${date} @${location}`}
-                        />
-                        <CardText>
-                            {description}
-                        </CardText>
-                    </Card>
-                    <br/>
-                </Row>
-            </Col>
+            <div>
+                {listOfFlyersLiked}
+            </div>
         )
     }
 }
 
 function mapStateToProps(state){
     return{
-        user: state.user
+        user: state.user,
+        flyers: state.data.events
     }
 }
 
