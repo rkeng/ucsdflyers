433a68af8841d06edb876562b17adb24807dee02
jil570 <jil570@ucsd.edu>
2017-03-12 22:13:14 -0700

    email

--- src/DumbComponents/RecruitmentNote.js
+++ src/DumbComponents/RecruitmentNote.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
+import AnimakitExpander from 'animakit-expander';
+import { Button } from 'react-bootstrap'
 import { FaCalendar, FaGroup, FaEnvelope, FaStreetView } from 'react-icons/lib/fa';
-import { Link } from 'react-router'
 import { ColCenter } from '../Commen'
 
 class RecruitmentNote extends React.Component {
 
-    render(){
+  constructor(props){
+    super(props);
+    this.state={
+      expanded: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    this.setState(prevState => ({
+      expanded: !prevState.expanded
+    }));
+  }
+
+  render(){
       const { clubName, seeking, dueDate, email, description } = this.props.data
       return (
-            <Panel bsStyle='info' header={clubName}>
-              <ColCenter>
-                  <h5><FaStreetView/> {seeking}</h5>
-                  <h5><FaCalendar/> Due date: {dueDate} <br/></h5>
-                  <h5><FaGroup/> Organization: {clubName} <br/></h5>
-                  <h5><FaEnvelope/> Email: {email} <br/></h5>
-                  <p>{description}</p>
+        <Panel bsStyle='info' header={clubName}>
+          <ColCenter>
+              <h5><FaStreetView/> {seeking}</h5>
+              <h5><FaCalendar/> Due date: {dueDate} <br/></h5>
+              <h5><FaGroup/> Organization: {clubName} <br/></h5>
+              <h5><FaEnvelope/> Email: {email} <br/></h5>
+              <p>{description}</p>
 
-                  <Link className='btn btn-success'>Apply Now!</Link>
-              </ColCenter>
-            </Panel>
-        )
+              <Button onClick={this.handleClick}>{this.state.expanded ? 'Cancel' : 'Apply Now'}</Button>
+              <AnimakitExpander expanded={this.state.expanded}>
+                  <div className="text">
+                    Send Email to <a href="mailto:">{email}</a>
+                  </div>
+              </AnimakitExpander>
+          </ColCenter>
+        </Panel>
+      )
     }
 }
 
